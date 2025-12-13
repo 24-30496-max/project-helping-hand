1de19501a8159c81c8b2221a95d5fe5aa702b9f4
Andrei G. Gutierrez <24-30496@g.batstate-u.edu.ph>
2025-12-13 22:47:01 +0800

    Remove duplicate app.run call

--- app.py
+++ app.py
@@ -883,4 +883,3 @@ if __name__ == "__main__":
         db.create_all()
         create_admin_accounts()
     app.run(host='127.0.0.1', port=5000, debug=True)
-    app.run(host="127.0.0.1", port=5000, debug=True)
